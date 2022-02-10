c2b6c9889aee62154ffadabe78833aca96fdc758
Gr3g03 <71180086+Gr3g03@users.noreply.github.com>
2022-02-10 15:18:41 +0100

    server added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
 
+  type Quotes = {
+    id: number
+    quote: string
+    author: string
+  }
+
+
+  const [quotes, setQuotes] = useState<Quotes[]>([])
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/quotes`)
+      .then(resp => resp.json())
+      .then(quotesFromServer => setQuotes(quotesFromServer))
+  }, [])
+
+
   return (
     <div className="App">
+      {
+        quotes.map(quote =>
+          <blockquote key={quote.id} className='quoteContainer'>
 
+            <p className='quote'>
+              {quote.quote}
+            </p>
+            <h3 className='author'>
+              {quote.author}
+            </h3>
+
+          </blockquote>
+        )}
     </div>
   )
 }
